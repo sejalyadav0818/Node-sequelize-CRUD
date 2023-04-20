14184cb03bbe7d85b2ec84d47e12793d4dcb18bd
SEJAL YADAV <130818890+sejalyadav0818@users.noreply.github.com>
2023-04-20 08:33:33 +0530

    Update app.js
    Search & sort implemented

--- app.js
+++ app.js
@@ -145,5 +145,113 @@ app.delete("/users/:id", async (req, res) => {
   }
 });
 
+
+// // Get all users with searching
+// app.get("/users", async (req, res) => {
+//   try {
+//     const {searchBy = 'email', searchTerm = ''} = req.query;
+//     const users = await User.findAll({
+//       where: {
+//         [searchBy]: {
+//           [Op.like]: `%${searchTerm}%`
+//         }
+//       }
+//     });
+//     res.json({
+//       data: {
+//         users: users,
+//       },
+//       error: null,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({
+//       data: null,
+//       error: {
+//         success: false,
+//         message: "Unable to retrieve users",
+//         errorMessage: error.message,
+//       },
+//     });
+//   }
+// });
+
+
+// Get all users with optional search by name
+app.get("/users", async (req, res) => {
+  try {
+    const { search } = req.query;
+    const queryOptions = {};
+    
+    if (search) {
+      queryOptions.where = { name: { [Op.like]: `%${search}%` } };
+    }
+
+    const users = await User.findAll(queryOptions);
+    res.json({
+      data: {
+        users: users,
+      },
+      error: null,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      data: null,
+      error: {
+        success: false,
+        message: "Unable to retrieve users",
+        errorMessage: error.message,
+      },
+    });
+  }
+});
+// queryOptions.where = { name: { [Op.like]: `%${search}%` } }; we can importve any other query we can use
+
+// Yes, Sequelize supports many other query options that can be used to perform different types of queries. Some of the commonly used query options include:
+
+// Op.eq: Matches values that are equal to the given value.
+// Op.ne: Matches values that are not equal to the given value.
+// Op.gt: Matches values that are greater than the given value.
+// Op.gte: Matches values that are greater than or equal to the given value.
+// Op.lt: Matches values that are less than the given value.
+// Op.lte: Matches values that are less than or equal to the given value.
+// Op.between: Matches values that are between the given range.
+// Op.notBetween: Matches values that are not between the given range.
+// Op.in: Matches values that are in the given array.
+// Op.notIn: Matches values that are not in the given array.
+// Op.like: Matches values that are like the given pattern.
+// Op.notLike: Matches values that are not like the given pattern.
+// You can refer to the Sequelize documentation for more information on available query options: https://sequelize.org/master/manual/querying.html#operators
+
+// Get all users
+// GET http://localhost:3000/users?searchBy=email&searchTerm=john
+
+app.get("/users", async (req, res) => {
+  try {
+    const { sortBy } = req.query;
+    const users = await User.findAll({
+      order: sortBy ? [[sortBy, "ASC"]] : undefined,
+    });
+    res.json({
+      data: {
+        users: users,
+      },
+      error: null,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      data: null,
+      error: {
+        success: false,
+        message: "Unable to retrieve users",
+        errorMessage: error.message,
+      },
+    });
+  }
+});
+// http://localhost:3000/users?sort=age.desc
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
